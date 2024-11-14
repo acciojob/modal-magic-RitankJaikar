9c2e5851f0c42b62033b6ee600a8d8309809d912
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-14 19:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const modal= document.querySelector(".modal");
+const openButton= document.querySelector("#openModal");
+const closeButton= document.querySelector(".close-modal");
+const modalContent= document.querySelector(".modal-content");
+
+openButton.addEventListener("click", event => {
+	modal.style.display= "block";
+	setTimeout(() => {
+		modalContent.classList.add("show");
+	}, 10)
+})
+
+closeButton.addEventListener("click", event => {
+	modal.style.display= "none";
+	modalContent.classList.remove("show");
+})
+
+modal.addEventListener("click", event => {
+	if(event.target === modal) {
+		modal.style.display= "none";
+		modalContent.classList.remove("show");
+	}
+})
+
+
+
